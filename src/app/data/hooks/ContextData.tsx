d6277683c5870381c28d0f70405f94be64320edb
jgbenine <joaobenine@gmail.com>
2024-03-17 17:16:08 -0300

    remove contextsData

--- src/app/data/hooks/ContextData.tsx
+++ src/app/data/hooks/ContextData.tsx
@@ -1,119 +1,22 @@
-"use client";
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useEffect,
-  useState,
-} from "react";
-import { fetchDefault } from "../api/axiosConfig";
+// 'use client'
 
-export interface CharacterData {
-  id: number;
-  image: string;
-  name: string;
-  gender: string;
-  status: string;
-  species: string;
-  type?: string;
-  episodes?: string[];
-  location?: {
-    name: string;
-    url: string;
-  };
-  origin?: {
-    name: string;
-    url: string;
-  };
-}
-
-interface DataContextProps {
-  children: ReactNode;
-}
-
-interface DataContextValue {
-  characterDataFetch: CharacterData[];
-  handleLoadMore: () => void;
-  isLoading: boolean;
-  setSearchInputQuery: Dispatch<SetStateAction<string>>;
-  searchCharacterData: CharacterData[];
-}
-
-export const DataContext = createContext<DataContextValue | null>(null);
-
-export function ContextData({ children }: DataContextProps) {
-  const [characterDataFetch, setCharacterDataFetch] = useState<CharacterData[]>(
-    []
-  );
-  const [searchCharacterData, setSearchCharacterData] = useState<
-    CharacterData[]
-  >([]);
-  const [searchInputQuery, setSearchInputQuery] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const endpoint = searchInputQuery
-          ? `https://rickandmortyapi.com/api/character/?page=${currentPage}&name=${searchInputQuery}`
-          : `/character?page=${currentPage}`;
-  
-        const response = await fetchDefault(endpoint);
-        const data = response.data.results;
-  
-        const filterFunction = (prevData: CharacterData[]) => {
-          return (newResults: CharacterData[]) => {
-            return newResults.filter((newResult) => {
-              return !prevData.some(
-                (prevResult) =>
-                  JSON.stringify(prevResult) === JSON.stringify(newResult)
-              );
-            });
-          };
-        };
-  
-        if (searchInputQuery !== '') {
-          setCharacterDataFetch([])
-          setSearchCharacterData((prevData) => {
-            return currentPage === 1 ? data : [...prevData, ...data];
-          });
-          setCharacterDataFetch([]); // Limpar os dados normais
-        } else {
-          setSearchCharacterData([]); // Limpar os dados de pesquisa
-          setCharacterDataFetch((prevData) => [
-            ...prevData,
-            ...filterFunction(prevData)(data),
-          ]);
-        }
-  
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Request error", error);
-      }
-    }
-  
-    fetchData();
-  }, [currentPage, searchInputQuery]);
-
-
-  function handleLoadMore() {
-    setIsLoading(false);
-    setCurrentPage((prevPage) => prevPage + 1);
-  }
-
-  return (
-    <DataContext.Provider
-      value={{
-        characterDataFetch,
-        handleLoadMore,
-        isLoading,
-        setSearchInputQuery,
-        searchCharacterData,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
-}
+// interface CharacterData {
+//   data: {
+//     id: number;
+//     image: string;
+//     name: string;
+//     gender: string;
+//     status: string;
+//     species: string;
+//     type?: string;
+//     episodes?: string[];
+//     location?: {
+//       name: string;
+//       url: string;
+//     };
+//     origin?: {
+//       name: string;
+//       url: string;
+//     };
+//   }[];
+// }
